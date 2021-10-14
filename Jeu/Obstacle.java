3676680cc548793150a9697f99f71ceb3243bc36
DySQRD <dylan.toledano@tutanota.com>
2021-10-14 21:26:21 +0200

    Packages extraits du src de Marc

---
+++ Jeu/Obstacle.java
@@ -0,0 +1,9 @@
+package Jeu;
+
+public class Obstacle {
+    public float height;
+    public float width;
+    public String name;
+    public float x;
+    public float y;
+}
--- src/Jeu/Obstacle.java
+++
@@ -1,9 +0,0 @@
-package Jeu;
-
-public class Obstacle {
-    public float height;
-    public float width;
-    public String name;
-    public float x;
-    public float y;
-}
