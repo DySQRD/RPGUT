79f489a9c3e40c1082d566f0390506cd994bbebb
DySQRD <dylan.toledano@tutanota.com>
2021-10-25 16:54:38 +0200

    Passage des méthodes de BD en static
    Etant donné qu'il n'y a qu'une seule connexion par lancement de programme, pas besoin de créer d'instance de la classe BD.

--- Joueur.java
+++ Joueur.java
@@ -4,22 +4,10 @@ import java.util.HashMap;
 public class Joueur extends Entite {
 	private String pseudo;
 	
-	public Joueur(String pseudo, int id, int xp, int pv, int attaque, int vitesse, ArrayList<Consommable> inventaire) {
-		this.id = id;
+	public Joueur(int id, int xp, HashMap<String, Integer> stats, ArrayList<Consommable> inventaire, String pseudo) {
+		super(id, xp, stats, inventaire);
 		this.pseudo = pseudo;
-		this.xp = xp;
-		this.stats = new HashMap<String, Integer>();
-		this.inventaire = inventaire;
 	}
-	
-	public Joueur(String pseudo, int id, int xp, HashMap<String, Integer> stats, ArrayList<Consommable> inventaire) {
-		this.id = id;
-		this.pseudo = pseudo;
-		this.xp = xp;
-		this.stats = stats;
-		this.inventaire = inventaire;
-	}
-	
 	public String getPseudo() {
 		return pseudo;
 	}
