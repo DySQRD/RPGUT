3afbf49c4c7bb7b5d0956c89ea6fb911526eaba9
Souuux <92717439+Souuux@users.noreply.github.com>
2021-11-21 18:19:01 +0100

    Update Monstre.java

--- src/Monstre.java
+++ src/Monstre.java
@@ -13,4 +13,8 @@ public class Monstre extends Entite {
 	public boolean dropObjet(int chance) { //Retourne si l'objet est lach� ou non 
 		return (Math.random()*100<100);
 	}
+	//ajout d'une methode pour le drop des consomable a partir de l'invetaire 
+	public void removeOject(int id) {
+        this.inventaire.removeIf(consommable -> consommable.getId().equals(id));
+    }
 }
